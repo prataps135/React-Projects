48b7ac5567abee8e3ba41937e766b713c9c27077
Pratap Singh Sisodia <48864550+prataps135@users.noreply.github.com>
2023-10-02 16:46:17 +0530

    Brand page project added.

---
+++ Brand Page/src/components/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./App.css";
+
+function App() {
+
+
+  return (
+    <>
+      Hello world
+    </>
+  )
+}
+
+export default App
